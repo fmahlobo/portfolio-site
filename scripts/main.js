f8915ba810fd1a552adf8720be24dfe72b1d83f2
fortunate <fmahlobo91@gmail.com/>
2025-03-04 09:26:54 +0200

    section links

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+
+
+function about(event) {
+    // If needed, prevent default behavior
+    event.preventDefault();
+
+    // Redirect the user to another page
+    window.location.href = "about.html";
+}
+
+// Add event listener to div
+document.getElementById("about").addEventListener("click", about);
+
+
+
+
+function experience(event) {
+    // If needed, prevent default behavior
+    event.preventDefault();
+
+    // Redirect the user to another page
+    window.location.href = "experience.html";
+}
+
+// Add event listener to div
+document.getElementById("experience").addEventListener("click", experience);
+
+
+
+function education(event) {
+    // If needed, prevent default behavior
+    event.preventDefault();
+
+    // Redirect the user to another page
+    window.location.href = "education.html";
+}
+
+// Add event listener to div
+document.getElementById("education").addEventListener("click", education);
+
+
+
+function projects(event) {
+    // If needed, prevent default behavior
+    event.preventDefault();
+
+    // Redirect the user to another page
+    window.location.href = "projects.html";
+}
+
+// Add event listener to div
+document.getElementById("projects").addEventListener("click", projects);
